341fd2808417045a55e8ea0e812a1eebb0afef90
JosephPham324 <quangpnce170036@fpt.edu.vn>
2024-07-06 18:28:49 +0700

    Add authProtection higher order component

---
+++ src/Web/ClientApp/src/utils/withAuthProtection.js
@@ -0,0 +1,34 @@
+﻿import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+
+const withAuthProtection = (WrappedComponent) => {
+    return (props) => {
+        const { isAuthenticated } = useContext(AuthContext);
+
+        if (!isAuthenticated) {
+            return <div>You need to login to access this content.</div>;
+        }
+
+        return <WrappedComponent {...props} />;
+    };
+};
+
+export default withAuthProtection;
+
+//HOW TO USE
+
+//Sample component to be protected
+//const ProtectedComponent = () => {
+//    return <div>Protected Content</div>;
+//};
+
+
+//Import component and withAuthProtection in the file
+//import withAuthProtection from './path/to/withAuthProtection';
+//import ProtectedComponent from './path/to/ProtectedComponent';
+
+//Wrap the component with withAuthProtection
+//const ProtectedComponentWithAuth = withAuthProtection(ProtectedComponent);
+
+//Use the component like normal
+//<ProtectedComponentWithAuth />
