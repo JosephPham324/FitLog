89c1c891fe7c80123c7af16f4fdc481396652671
JosephPham324 <quangpnce170036@fpt.edu.vn>
2024-08-03 10:48:01 +0700

    Update after merge

--- src/Web/ClientApp/src/AppRoutes.js
+++ src/Web/ClientApp/src/AppRoutes.js
@@ -31,8 +31,8 @@ import RecoverAccount from './components/AccountRecovery/RecoverAccount'
 import RecoverConfirmation from './components/AccountRecovery/RecoverConfirmation'
 import RecoverInitiate from './components/AccountRecovery/RecoverInitiate'
 import ConfirmEmail from './components/EmailConfirmation/ConfirmEmail'
-import { WorkoutProgramsPage } from './components/WorkoutProgramsPage';
-import { WorkoutProgramsDetail } from './components/WorkoutProgramsDetail';// Correct the import statement
+import WorkoutProgramsPage from './components/WorkoutProgramsPage';
+import WorkoutProgramsDetail from './components/WorkoutProgramsDetail';// Correct the import statement
 import CoachProfile from './components/CoachProfile';
 import WorkoutLogPage from './page/WorkoutLog/CreateWorkoutLog/WorkoutLog';
 import CreateWorkoutTemplatePage from './page/WorkoutLog/CreateWorkoutTemplate/CreateWorkoutTemplate';
